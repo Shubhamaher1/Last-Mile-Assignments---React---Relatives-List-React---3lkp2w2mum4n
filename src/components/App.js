92260fef26eb54826ce2b8f8a977bdcdcb6461a4
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-03-07 18:12:27 +0530

    ul

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+let list = ["shubham", "aher", "party"];
+function App() {
+  const [data, setdata] = useState(list);
+  return (
+    <div className="main">
+      <ol>
+        {data.map((ele, idex) => {
+          return <li key={"relativeListItem" + idex + 1}>{ele}</li>;
+        })}
+      </ol>
+    </div>
+  );
 }
-
 
 export default App;
